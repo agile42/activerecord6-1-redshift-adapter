6ed14fe9bb4961eac6b8df28f6cd15067d761460
Ben Keeping <benjaminkeeping@gmail.com>
2021-01-29 20:13:38 +0000

    Support rails 6.1.1

--- lib/active_record/connection_adapters/redshift/column.rb
+++ lib/active_record/connection_adapters/redshift/column.rb
@@ -3,8 +3,12 @@ module ActiveRecord
     class RedshiftColumn < Column #:nodoc:
       delegate :oid, :fmod, to: :sql_type_metadata
 
-      def initialize(name, default, sql_type_metadata, null = true, default_function = nil)
+      def initialize(name, default, sql_type_metadata = nil, null = true, default_function = nil, collation: nil, comment: nil, **)
         super name, default, sql_type_metadata, null, default_function
+      end
+
+      def array?(**)
+        false
       end
     end
   end
